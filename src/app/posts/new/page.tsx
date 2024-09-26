5381e986b102ff6dcc6a2665132080aaf6d2bade
limgunny <red373712345@gmail.com>
2024-09-26 17:08:19 +0900

    1st

---
+++ src/app/posts/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return (
+    <div>
+      <h1> New Post Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic assumenda
+        consequuntur itaque esse omnis! Magnam, quo! Nihil, commodi! Eos
+        perspiciatis, ad quia incidunt ullam dicta architecto dolor quisquam
+        tempora soluta.
+      </p>
+    </div>
+  )
+}
